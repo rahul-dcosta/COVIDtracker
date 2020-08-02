53a087293b1bb010e0f3ec6b92b4d14b04e343b7
rdcosta <rdcosta@umich.edu>
2020-08-02 14:13:32 +0400

    added components and api

--- covidtracker/src/App.js
+++ covidtracker/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+// import Cards from './components/Cards/Cards'
+// import Chart from './components/Chart/Chart'
+// import CountryPicker from './components/CountryPicker/CountryPicker'
+
+import {Cards, Chart, CountryPicker} from './components';
+import styles from './App.module.css';
+import {fetchData} from './api'
+
 class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+  }
   render() {
     return(
-      <div>
-        <h1>App</h1>
+      <div className ={styles.container}>
+        <Cards />
+        <Chart />
+        <CountryPicker />
       </div>
     )
   }
