801a1d8e3caabd6243bca14513757cf40a3cde03
rdcosta <rdcosta@umich.edu>
2020-08-07 13:51:33 +0400

    countries succesfully change

--- covidtracker/src/App.js
+++ covidtracker/src/App.js
@@ -12,19 +12,26 @@ class App extends React.Component {
 
   state = {
     data: {},
+    country: '',
   }
 
   async componentDidMount() {
     const fetchedData = await fetchData();
-    this.setState({data: fetchedData})
+    this.setState({data: fetchedData});
   }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({data: fetchedData, country: country});
+  }
+
   render() {
     const {data} = this.state;
     return(
       <div className ={styles.container}>
         <Cards data = {data}/>
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart />
-        <CountryPicker />
       </div>
     )
   }
